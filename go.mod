f9bd6646020260f456419d8af3178d6ff83fa02c
gwest <gwest@gwest@redhat.com-mac>
2021-07-08 16:47:51 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/glennswest/zombiestorm
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/kardianos/service v1.2.0
+)
---
+++ zombiestorm.go
@@ -0,0 +1,87 @@
+package main
+
+import "github.com/gin-gonic/gin"
+import "fmt"
+import "strconv"
+import "time"
+//import "net/http"
+import "github.com/kardianos/service"
+import "log"
+
+
+
+var logger service.Logger
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	// Do work here
+        zombieserver();
+}
+
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "zombiestorm",
+		DisplayName: "ZombieStorm",
+		Description: "ZombieStorme",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
+
+func zombieserver() {
+	//r := gin.Default();
+        r := gin.New()
+        r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
+
+	r.GET("/healthz", func(c *gin.Context) {
+                time.Sleep(3 * time.Second)
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func IsNumeric(s string) bool {
+   _, err := strconv.ParseFloat(s, 64)
+   return err == nil
+}
+ 
