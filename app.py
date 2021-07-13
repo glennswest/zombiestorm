a4bb815cd2da3e646381adc0fd65ce68859dce06
gwest <gwest@gwest@redhat.com-mac>
2021-07-13 09:46:35 -0500

    Change to fastest zombie rate

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import time
 import subprocess
 
 print("Starting Python ZombieStorm")
-cmd_str = "sleep 5"
 print("Spawning Zombies")
 proc = subprocess.Popen([cmd_str], shell=True,
              stdin=None, stdout=None, stderr=None, close_fds=True)
