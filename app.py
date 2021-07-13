70e76785bdeff97ab6cb1a9bcb48fbdef55fae02
gwest <gwest@gwest@redhat.com-mac>
2021-07-13 09:31:16 -0500

    Add more zombies and logging

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import time
 import subprocess
 
+print("Starting Python ZombieStorm")
 cmd_str = "sleep 5"
+print("Spawning Zombies")
 proc = subprocess.Popen([cmd_str], shell=True,
              stdin=None, stdout=None, stderr=None, close_fds=True)
 proc = subprocess.Popen([cmd_str], shell=True,
              stdin=None, stdout=None, stderr=None, close_fds=True)
 proc = subprocess.Popen([cmd_str], shell=True,
              stdin=None, stdout=None, stderr=None, close_fds=True)
-time.sleep(9000)
+proc = subprocess.Popen([cmd_str], shell=True,
+             stdin=None, stdout=None, stderr=None, close_fds=True)
+proc = subprocess.Popen([cmd_str], shell=True,
+             stdin=None, stdout=None, stderr=None, close_fds=True)
+time.sleep(90000000)
