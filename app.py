bc6a22d3f4590ae58d468aeedeb811e98627f779
gwest <gwest@gwest@redhat.com-mac>
2021-07-13 10:02:01 -0500

    Add trap of sigint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import subprocess
 
 print("Starting Python ZombieStorm")
 print("Spawning Zombies")
-cmd_str="sleep 3"
+cmd_str="trap -- '' SIGINT SIGTERM;sleep 3"
 proc = subprocess.Popen([cmd_str], shell=True,
              stdin=None, stdout=None, stderr=None, close_fds=True)
 proc = subprocess.Popen([cmd_str], shell=True,
